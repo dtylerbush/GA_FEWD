165a357e0449939105abf15c8faa3f919960aadb
dtylerbush <dtylerbush@gmail.com>
2022-10-05 18:59:15 -0400

    trying again 10-05

---
+++ rock-paper-scissors.js
@@ -0,0 +1,24 @@
+
+
+
+
+/* Player 1 and 2
+ count "1" while making fist, 
+ count "2" while making flat hand
+ count "3" while holding out two fingers /*
+
+ /* Player 1 and 2
+ chooses Answer
+
+If Player 1 Answer = ?
+If Player 2 Answer = ?
+
+ Answer =
+/* Rock > Scissor */
+/* Scissor > Paper */
+/* Paper > Rock */
+
+/* Rock = Rock (repeat) */
+/* Scissor = Scissor (repeat) */
+/* Paper = Paper (repeat) */
+
