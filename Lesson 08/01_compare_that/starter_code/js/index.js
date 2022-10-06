a6e7a09d71154c6cdeb7011e6ab9388f9f1610c9
dtylerbush <dtylerbush@gmail.com>
2022-10-05 22:05:06 -0400

    During class 10-05

---
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -0,0 +1,26 @@
+
+ document.querySelector('#submit').onclick = function(e) {
+    e.preventDefault();
+     let comparison;
+     let a = Number(document.querySelector('#a').value);
+     let b = Number(document.querySelector('#b').value);
+ 
+     // Add comparison logic
+
+    //  Are the not numbers
+    if(isNaN(a) || isNaN(b)){
+      comparison = "N/A";
+    } else if (a > b){
+      comparison = ">";
+    } else if(a < b){
+      comparison = "<";
+    } else{
+      comparison = "=";
+    } 
+  
+  
+
+ 
+     document.querySelector('#comparison').innerText = comparison;
+   };
+ 
