58288908c99a6e895e39bea3b1b821186dfa9c98
dtylerbush <dtylerbush@gmail.com>
2022-11-12 11:38:53 -0500

    Update 11-12

--- Lesson 18/01_weather_api/starter_code/js/script.js
+++ Lesson 18/01_weather_api/starter_code/js/script.js
@@ -7,21 +7,53 @@
 // Display the results in the #weather-results div
 // Each new search should erase the previous results
 
-const myApiKey = "paste your API key here";
+const myApiKey = "0d5c31ad06a49bb6ee7011f33b206bc3";
 
 async function handleWeatherRequest(e) {
+
+  e.preventDefault();
   
   // get user input value from textbox
+
+  let city = document.querySelector('input[name="city"]').value;
+
   
   // build the url address with YOUR personal API key and the users input city
+
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${myApiKey}`;
+
+  const apiURLalternative = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&APPID=" + "&q=" + myApiKey;
   
   // make an API call using fetch() and capture the response in a variable
- 
+
+  const response = await fetch(apiURL);
+
   // convert the response to a readable json format with .json()
+
+  const convert = await response.json();
   
   // print your data to the console to see its format, dont forget to delete later
+
+  console.log(convert);
   
   // Write a separate function to convert the temps from the default units (Kelvin) to a more readable F or C
+
+  
+
+  const currentTemp = convertKtoF(convert.main.temp);
+  const feelsLikeTemp = convertKtoF(convert.main.feels_like);
+  const humidity = convert.main.humidity;
+  const pressure = convert.main.pressure;
+  const highTemp = convertKtoF(convert.main.temp_max);
+  const lowTemp = convertKtoF(convert.main.temp_min);
+
+  const weatherResults = `<li>The temperature in ${convert.name} is currently ${currentTemp}&#8457;</li>
+  <li>Feels Like: ${feelsLikeTemp}&#8457</li>
+  <li>Humidity: ${humidity}%</li>
+  <li>Pressure: ${pressure}</li>
+  <li>High: ${highTemp}&#8457</li>
+  <li>Low: ${lowTemp}&#8457</li>`;
+
   
   // build the icon src
   
@@ -32,10 +64,22 @@ async function handleWeatherRequest(e) {
   // clear out previous icon img
   
   // print all results to dom
+
+  document.getElementById('weather-results').innerHTML = weatherResults;
   
   // append icon img to dom
   
+
 }
 
+function convertKtoF(kelvinTemp){
+  let tempF = (kelvinTemp - 273) * 1.8 + 32;
+
+  tempF = Math.round(tempF);
+  return tempF;
+}
 
 // Attach an event listener to the submit button
+
+document.querySelector('#weather-search').addEventListener('submit', handleWeatherRequest);
+
